fc61ea197076ed3d248296ce5b9ae8cbcd45748b
Mikhail Kovalev <mikhail@kovalev.best>
2022-10-04 11:52:25 +0200

    Fixed multiboot2 header and linking

--- loader/main.c
+++ loader/main.c
@@ -1,7 +1,19 @@
 #include <multiboot2.h>
 #include <stdint.h>
+#include <misc.h>
+
+void print_str(char * str)
+{
+    while (*str != '\0') { 
+        printc(*str);
+        ++str;
+    }
+    return;
+}
 
 void main(unsigned long magic, unsigned long addr)
 {
     /* Initialize everything */
+    print_str("Hello from loader!\n");
+    while (1) ;
 }
