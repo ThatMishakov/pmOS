e31e7ddc583565260633ab6b11b193671bcafcdd
Mikhail Kovalev <mkm12342001@gmail.com>
2022-10-07 12:24:40 +0200

    Added inicialization of temporary paging structures

--- kernel/common/memory.h
+++ kernel/common/memory.h
@@ -10,22 +10,21 @@ typedef struct {
 
 typedef struct
 {
-    uint8_t present                   :1;
-    uint8_t writeable                 :1;
+    uint8_t present                   :1; 
+    uint8_t writeable                 :1; 
     uint8_t user_access               :1;
     uint8_t write_through             :1;
-    uint8_t cache_disabled            :1;
+    uint8_t cache_disable             :1;
     uint8_t accessed                  :1;
-    uint8_t global                    :1;
-    uint8_t size                      :1; // Must be 0
+    uint8_t ignored                   :1;
+    uint8_t reserved                  :1; // Must be 0
     uint8_t avl                       :3;  // Avaliable
     uint8_t pat                       :1;
-    uint64_t page_ppn                   :28;
-    uint64_t reserved_1                 :12; // must be 0
+    uint64_t page_ppn                   :40;
     uint64_t avl2                       :7;
     uint8_t  pk                         :4;
     uint8_t execution_disabled       :1;
-} PACKED PML4;
+} PACKED PML4E;
 
 typedef struct
 {
@@ -35,16 +34,15 @@ typedef struct
     uint8_t write_through             :1;
     uint8_t cache_disabled            :1;
     uint8_t accessed                  :1;
-    uint8_t global                    :1;
-    uint8_t size                      :1; 
+    uint8_t ignored                   :1;
+    uint8_t size                      :1;  // For 1 GB pages 
     uint8_t avl                       :3;  // Avaliable
-    uint8_t pat                       :1;
-    uint64_t page_ppn                   :28;
-    uint64_t reserved_1                 :12; // must be 0
-    uint64_t avl2                       :7;
-    uint8_t  pk                         :4;
-    uint8_t execution_disabled       :1;
-} PACKED PDPT;
+    uint8_t ignored1                  :1;
+    uint64_t page_ppn                 :40;
+    uint64_t avl2                     :7;
+    uint8_t  pk                       :4;
+    uint8_t execution_disabled        :1;
+} PACKED PDPTE;
 
 typedef struct
 {
@@ -54,17 +52,34 @@ typedef struct
     uint8_t write_through             :1;
     uint8_t cache_disabled            :1;
     uint8_t accessed                  :1;
-    uint8_t global                    :1;
+    uint8_t dirty                     :1;
     uint8_t size                      :1; 
     uint8_t avl                       :3;  // Avaliable
-    uint8_t pat                       :1;
-    uint64_t page_ppn                   :28;
-    uint64_t reserved_1                 :12; // must be 0
-    uint64_t avl2                       :7;
-    uint8_t  pk                         :4;
+    uint8_t ignored                   :1;
+    uint64_t page_ppn                 :40;
+    uint64_t avl2                     :7;
+    uint8_t  pk                       :4;
     uint8_t execution_disabled       :1;
-} PACKED PD;
+} PACKED PDE;
 
+typedef struct
+{
+    uint8_t present                   :1;
+    uint8_t writeable                 :1;
+    uint8_t user_access               :1;
+    uint8_t write_through             :1;
+    uint8_t cache_disabled            :1;
+    uint8_t accessed                  :1;
+    uint8_t dirty                     :1;
+    uint8_t pat                       :1;
+    uint8_t global                    :1; 
+    uint8_t avl                       :2;  // Avaliable
+    uint8_t ignored                   :1;
+    uint64_t page_ppn                 :40;
+    uint64_t avl2                     :7;
+    uint8_t  pk                       :4;
+    uint8_t execution_disabled       :1;
+} PACKED PTE;
 
 
 typedef struct {
@@ -78,8 +93,19 @@ typedef struct {
 } PACKED Page_Table_Argumments;
 
 typedef struct {
-    PML4 entries[512];
-} PACKED ALIGNED(0x1000) Page_Table;
+    PML4E entries[512];
+} PACKED ALIGNED(0x1000) PML4;
 
+typedef struct {
+    PDPTE entries[512];
+} PACKED ALIGNED(0x1000) PDPT;
+
+typedef struct {
+    PDE entries[512];
+} PACKED ALIGNED(0x1000) PD;
+
+typedef struct {
+    PTE entries[512];
+} PACKED ALIGNED(0x1000) PT;
 
 #endif
--- loader/paging.c
+++ loader/paging.c
@@ -1,7 +1,3 @@
 #include <paging.h>
 #include "../kernel/common/memory.h"
 #include <stdint.h>
-
-Page_Table loader_page_table =
-        {(Page_Table_Entry){},
-         (Page_Table_Entry{0})};
