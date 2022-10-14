0fe3e39c7bb074453c574b620c078302e312f4c3
Mikhail Kovalev <mikhail@kovalev.best>
2022-10-14 18:10:19 +0200

    Changed free_pages_list to a pointer

--- kernel/free_page_alloc.cc
+++ kernel/free_page_alloc.cc
@@ -2,15 +2,17 @@
 #include "lib/list.hh"
 #include "misc.hh"
 
-List<uint64_t> free_pages_list;
+List<uint64_t>* free_pages_list = nullptr;
 
 void free_page_alloc_init()
 {
+    free_pages_list = new List<uint64_t>;
+
     uint64_t start = (uint64_t)unoccupied;
     uint64_t size = FREE_PAGES_SIZE*4096;
 
     unoccupied = (void*)((uint64_t)unoccupied + size);
 
     for (int i = 0; i < size; ++i)
-        free_pages_list.push_back(i+start);
+        free_pages_list->push_back(i+start);
 }
