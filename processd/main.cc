d77771b6296ff1e35ab1b8abda2fa3b151719da9
Mikhail Kovalev <mikhail@kovalev.best>
2022-11-04 23:13:10 +0100

    Added latest changes

--- processd/main.cc
+++ processd/main.cc
@@ -1,9 +1,11 @@
 #include <stdlib.h>
+#include <kernel/types.h>
 
 extern "C" int main() {
-    int* t = (int*)malloc(sizeof(int));
-    *t = 123;
-    free((void*)t);
+    char* k = (char*)malloc(GB(1));
+    k[MB(100)] = 1;
+    k[MB(200)] = 2;
+    k[MB(300)] = 3;
 
     return 0;
 }
