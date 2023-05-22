661e9e296585a14c06a9245d618159810f603c10
Mikhail Kovalev <mkm12342001@gmail.com>
2023-05-22 17:17:13 +0200

    Moved libc source location

--- vfsd/file_descriptor.h
+++ vfsd/file_descriptor.h
@@ -13,14 +13,22 @@ struct Dir_Data {
     struct Path_Node *child_root;
 };
 
+struct File_Data {
+    uint64_t memory_object;
+
+};
+
+struct Filesystem;
+
 struct File_Descriptor {
     uint64_t id;
     size_t refcount;
+    struct Filesystem *filesystem;
 
     enum Descriptor_Type type;
     union {
         Dir_Data dir_data;
-
+        File_Data file_data;
     };
 };
 
