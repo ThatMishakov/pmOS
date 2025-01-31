56dfb6d3737dcab7148cc9f0243ac25582c630eb
Mikhail Kovalev <mkm12342001@gmail.com>
2025-01-31 02:21:24 +0100

    (hopefully) Fix some inline assembly

--- kernel/arch/i686/x86_asm.hh
+++ kernel/arch/i686/x86_asm.hh
@@ -3,11 +3,11 @@
 inline unsigned long getCR0()
 {
     unsigned long val;
-    asm("movl %%cr0, %0" : "=r"(val));
+    asm("movl %%cr0, %0" : "=r"(val) :: "memory");
     return val;
 }
 
-inline void setCR0(unsigned long val) { asm("movl %0, %%cr0" : : "r"(val)); }
+inline void setCR0(unsigned long val) { asm("movl %0, %%cr0" : : "r"(val) : "memory"); }
 
 inline unsigned long getCR2()
 {
@@ -21,18 +21,18 @@ inline void setCR3(unsigned long val) { asm("movl %0, %%cr3" : : "r"(val) : "mem
 inline unsigned long getCR3()
 {
     unsigned long val;
-    asm("movl %%cr3, %0" : "=r"(val));
+    asm("movl %%cr3, %0" : "=r"(val) :: "memory");
     return val;
 }
 
 inline unsigned long getCR4()
 {
     unsigned long cr4;
-    asm("movl %%cr4, %0" : "=r"(cr4));
+    asm("movl %%cr4, %0" : "=r"(cr4) :: "memory");
     return cr4;
 }
 
-inline void setCR4(unsigned long val) { asm("movl %0, %%cr4" : : "r"(val)); }
+inline void setCR4(unsigned long val) { asm("movl %0, %%cr4" : : "r"(val) : "memory"); }
 
 inline u64 read_msr(u32 reg)
 {
@@ -40,7 +40,7 @@ inline u64 read_msr(u32 reg)
     ulong ecx = reg;
     ulong edx;
 
-    asm volatile("rdmsr" : "=a"(eax), "=d"(edx) : "e"(ecx));
+    asm volatile("rdmsr" : "=a"(eax), "=d"(edx) : "e"(ecx) : "memory");
 
     return eax | ((u64)edx << 32);
 }
@@ -50,7 +50,7 @@ inline void write_msr(u32 reg, u64 val)
     ulong eax = val & 0xffffffff;
     ulong edx = val >> 32;
     ulong ecx = reg;
-    asm volatile("wrmsr" ::"a"(eax), "c"(ecx), "d"(edx));
+    asm volatile("wrmsr" ::"a"(eax), "c"(ecx), "d"(edx) : "memory");
 }
 
 inline u64 rdtsc()
@@ -65,6 +65,6 @@ inline void invlpg(void *m) { asm volatile("invlpg (%0)" : : "r"(m) : "memory");
 inline u16 str()
 {
     u16 val;
-    asm volatile("str %0" : "=rm"(val));
+    asm volatile("str %0" : "=rm"(val) :: "memory");
     return val;
 }
