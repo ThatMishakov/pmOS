d0a28514481351fde69137d71ee97012c83afbca
Mikhail Kovalev <mkm12342001@gmail.com>
2025-03-07 00:24:48 +0100

    Add newline character to uACPI initialization errors

--- devicesd/generic/acpi/acpi.c
+++ devicesd/generic/acpi/acpi.c
@@ -205,7 +205,7 @@ int acpi_init()
 {
     uacpi_status ret = uacpi_initialize(0);
     if (uacpi_unlikely_error(ret)) {
-        fprintf(stderr, "uacpi_initialize error: %s", uacpi_status_to_string(ret));
+        fprintf(stderr, "uacpi_initialize error: %s\n", uacpi_status_to_string(ret));
         return -ENODEV;
     }
 
@@ -213,14 +213,14 @@ int acpi_init()
 
     ret = uacpi_namespace_load();
     if (uacpi_unlikely_error(ret)) {
-        fprintf(stderr, "uacpi_namespace_load error: %s", uacpi_status_to_string(ret));
+        fprintf(stderr, "uacpi_namespace_load error: %s\n", uacpi_status_to_string(ret));
         return -ENODEV;
     }
 
 #if defined(__x86_64__) || defined(__i386__)
     ret = uacpi_set_interrupt_model(UACPI_INTERRUPT_MODEL_IOAPIC);
     if (uacpi_unlikely_error(ret)) {
-        fprintf(stderr, "uacpi_set_interrupt_model error: %s", uacpi_status_to_string(ret));
+        fprintf(stderr, "uacpi_set_interrupt_model error: %s\n", uacpi_status_to_string(ret));
         return -ENODEV;
     }
 #endif
@@ -230,13 +230,13 @@ int acpi_init()
 
     ret = uacpi_namespace_initialize();
     if (uacpi_unlikely_error(ret)) {
-        fprintf(stderr, "uacpi_namespace_initialize error: %s", uacpi_status_to_string(ret));
+        fprintf(stderr, "uacpi_namespace_initialize error: %s\n", uacpi_status_to_string(ret));
         return -ENODEV;
     }
 
     ret = uacpi_finalize_gpe_initialization();
     if (uacpi_unlikely_error(ret)) {
-        fprintf(stderr, "uACPI GPE initialization error: %s", uacpi_status_to_string(ret));
+        fprintf(stderr, "uACPI GPE initialization error: %s\n", uacpi_status_to_string(ret));
         return -ENODEV;
     }
 
